cacee7e5688f58eb572cd581ffcdf7a41ab56640
Levhcwm21. <alexags1983@gmail.com>
2026-06-06 11:17:15 +0530

    Add asyncio event loop setup in wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -3,6 +3,11 @@ from uvloop import install
 
 install()
 
+from asyncio import new_event_loop, set_event_loop
+
+bot_loop = new_event_loop()
+set_event_loop(bot_loop)
+
 from asyncio import sleep
 from urllib.parse import urlparse
 from contextlib import asynccontextmanager
@@ -15,9 +20,6 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from sabnzbdapi import SabnzbdClient
-from aioaria2 import Aria2HttpClient
-from aioqbt.client import create_client
-from aiohttp.client_exceptions import ClientError
 from aioqbt.exc import AQError
 
 from web.nodes import extract_file_ids, make_tree

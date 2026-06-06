455731f5294264baa5a31f48666869e388074bc2
Levhcwm21. <alexags1983@gmail.com>
2026-06-06 11:24:32 +0530

    Handle PeerIdInvalid exception and update send_rss call

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -14,6 +14,7 @@ from pyrogram.errors import (
     MediaEmpty,
     MediaCaptionTooLong,
     EntityBoundsInvalid,
+    PeerIdInvalid,
 )
 
 try:
@@ -98,6 +99,11 @@ async def send_message(message, text, buttons=None, block=True, photo=None, **kw
         return await send_message(message, text, None)
     except (MessageEmpty, EntityBoundsInvalid):
         return await send_message(message, text, parse_mode=ParseMode.DISABLED)
+    except PeerIdInvalid:
+        if isinstance(message, int):
+            await TgClient.bot.resolve_peer(message)
+            return await send_message(message, text, buttons, block, photo)
+        raise
     except Exception as e:
         LOGGER.error(str(e), exc_info=True)
         return str(e)
@@ -160,8 +166,7 @@ async def send_file(message, file, caption="", buttons=None):
 
 async def send_rss(text, chat_id, thread_id):
     try:
-        app = TgClient.user or TgClient.bot
-        return await app.send_message(
+        return await TgClient.bot.send_message(
             chat_id=chat_id,
             text=text,
             disable_web_page_preview=True,
@@ -171,7 +176,7 @@ async def send_rss(text, chat_id, thread_id):
     except (FloodWait, FloodPremiumWait) as f:
         LOGGER.warning(str(f))
         await sleep(f.value * 1.2)
-        return await send_rss(text)
+        return await send_rss(text, chat_id, thread_id)
     except Exception as e:
         LOGGER.error(str(e), exc_info=True)
         return str(e)
